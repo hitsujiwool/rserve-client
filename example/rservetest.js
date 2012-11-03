2049a79101687cc613f794a898f45b9a6ef010f6
hitsujiwool <utatanenohibi@gmail.com>
2012-11-03 23:13:10 +0900

    implemented Stream interface

--- example/rservetest.js
+++ example/rservetest.js
@@ -1,14 +1,3 @@
-require('../lib/rserve').connect('localhost', 6311, function(err, client) {
-    var command = 'a<-2.7+2';
-    console.log('evaluating', command);
-    client.eval(command, function(err, ans) {
-        console.log('answer', ans);
-
-        var command = 'a';
-        console.log('evaluating', command);
-        client.eval(command, function(err, ans) {
-            console.log('answer', ans);
-            client.end();
-        });
-    });
-});
+process.stdin
+    .pipe(require('../lib/rserve').connect('localhost', 6311))
+    .pipe(process.stdout);
